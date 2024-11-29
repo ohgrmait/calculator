cfae6fde59b1a57c905ad05e9d2b8dbc0101326f
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-11-29 21:09:50 +0530

    Add a function that performs a complete operation

--- js/script.js
+++ js/script.js
@@ -14,6 +14,27 @@ function divide(a, b) {
   return a / b;
 }
 
-let firstNumber = null;
-let operator = null;
-let secondNumber = null;
+function operate(firstNumber, operator, secondNumber) {
+  let result = null;
+  switch (operator) {
+    case "+":
+      result = add(firstNumber, secondNumber);
+      break;
+    case "-":
+      result = subtract(firstNumber, secondNumber);
+      break;
+    case "*":
+      result = multiply(firstNumber, secondNumber);
+      break;
+    case "/":
+      result = divide(firstNumber, secondNumber);
+      break;
+  }
+  return result;
+}
+
+let firstNumber = 3.34;
+let operator = "/";
+let secondNumber = -23;
+
+console.log(operate(firstNumber, operator, secondNumber));
