32dd632d52b1a69a14e8c0255b6238cdc1e939cd
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-11-29 20:58:46 +0530

    Add the basic math operators found in calculators

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("Hello, Calculator!");
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log(add(2, 3));
+console.log(subtract(3, -4));
+console.log(multiply(23, 56));
+console.log(divide(-23, -34));
