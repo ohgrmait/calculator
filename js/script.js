ea7e89bc597a0e97d347ff5c1107f7f07af73715
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-12-03 19:59:34 +0530

    Make calculator work using handleCalculation(arg)
    handleCalculation(arg) gets called from handleEvent(e) irrespective
    of the type of the event (whether it be pointer or keyboard).
    
    From the handleCalculation(arg) function, a switch statement makes the
    calculator work and allows for the following operations:
    - The calculate does not evaluate more than a single pair of numbers
    at a time (e.g., 12 + 7 - leading to an answer of 19 -), therefore,
    it does not adhere to the operator precedence.
    - Answers with long decimals are rounded off so that they don't overflow
    the display (some use a fixed set of significant figures and other long
    ones may be scientific notation).
    - Pressing "equals" or "any operator" before a calcualtion gives an
    error and the error has been customized in a fun way so that random
    words are thrown at the user for committing errors.

--- js/script.js
+++ js/script.js
@@ -33,18 +33,138 @@ function operate(firstNumber, operator, secondNumber) {
   return result;
 }
 
+function getRandomIntInclusive(min, max) {
+  // Taken from https://shorturl.at/TSjlB
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+function getRandomWords() {
+  const words = [
+    "NOOO!", "WHAT?", "Serious?",
+    "Holy Moly!", "Buhbye!", "Sleeping?"
+  ]
+  return words[getRandomIntInclusive(0, words.length - 1)];
+}
+
+function handleAllClear() {
+  result = null;
+  operator = null;
+  prevNumber = null;
+  currNumber = null;
+  minorDisplay.textContent = "";
+  majorDisplay.textContent = "0";
+}
+
+function handleEquals(arg) {
+  equalsPressed = true;
+  if (currNumber === null) {
+    minorDisplay.textContent = "";
+    majorDisplay.textContent = getRandomWords();
+    setTimeout(() => { handleAllClear(); }, 0.75 * 1000);
+    return;
+  }
+  if (operator !== null) {
+    result = operate(+prevNumber, operator, +currNumber);
+    if (result.toString().length > 10) {
+      result = result.toPrecision(4);
+    }
+    majorDisplay.textContent = result
+    prevNumber = result;
+    currNumber = null;
+  } else {
+    majorDisplay.textContent = currNumber;
+    minorDisplay.textContent = "";
+  }
+}
+
+function handleOperators(arg) {
+  if (prevNumber == null && currNumber == null) {
+    majorDisplay.textContent = getRandomWords();
+    setTimeout(() => { handleAllClear(); }, 0.75 * 1000);
+    return;
+  }
+  if (operator === null) {
+    if (equalsPressed) {
+      equalsPressed = false;
+      minorDisplay.textContent = "ANS";
+    }
+    prevNumber = currNumber;
+    currNumber = null;
+  } else if (currNumber !== null) {
+    minorDisplay.textContent = "ANS";
+    result = operate(+prevNumber, operator, +currNumber);
+    if (result.toString().length > 10) {
+      result = result.toPrecision(4);
+    }
+    majorDisplay.textContent = result;
+    prevNumber = result;
+    currNumber = null;
+  } else if (currNumber === null) {
+    if (equalsPressed) {
+      equalsPressed = false;
+      if (!majorDisplay.textContent.includes("ANS")) {
+        minorDisplay.textContent = "ANS";
+      }
+    } else {
+      minorDisplay.textContent = (
+        minorDisplay.textContent.slice(0, minorDisplay.textContent.length - 1)
+      );
+    }
+  }
+  minorDisplay.textContent += arg;
+  operator = arg;
+}
+
+function handleNumbers(arg) {
+  if (equalsPressed) {
+    equalsPressed = false;
+    handleAllClear();
+  }
+  minorDisplay.textContent += arg;
+  if (currNumber === null) {
+    currNumber = "";
+  }
+  currNumber += arg;
+}
+
+function handleCalculation(arg) {
+  switch (arg) {
+    case "0": case "1":
+    case "2": case "3":
+    case "4": case "5":
+    case "6": case "7":
+    case "8": case "9":
+      handleNumbers(arg);
+      break;
+    case "/": case "*":
+    case "-": case "+":
+      handleOperators(arg);
+      break;
+    case "=":
+      handleEquals(arg);
+      break;
+    case "AC":
+      handleAllClear();
+      break;
+  }
+}
+
 function handleEvent(e) {
   let arg = null;
   if (e.type === "click") {
     arg = e.target.textContent;
   }
-  calculate(arg);
+  handleCalculation(arg);
 }
 
-let result = null
+let result = null;
 let operator = null;
-let firstNumber = null;
-let secondNumber = null;
+let prevNumber = null;
+let currNumber = null;
+
+let equalsPressed = false;
 
 const buttons = document.querySelectorAll(".btn");
 const minorDisplay = document.querySelector(".minor-display");
